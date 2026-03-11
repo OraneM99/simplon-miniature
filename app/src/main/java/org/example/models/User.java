f8b0aa129e2ce0ca2405da4f120c0e67d2608e6b
CAILLET Amelie <185417630+AmeLIE2024@users.noreply.github.com>
2026-03-11 11:22:20 +0100

    feature: create class User

---
+++ app/src/main/java/org/example/models/User.java
@@ -0,0 +1,51 @@
+package org.example.models;
+
+public class User {
+
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+
+    private void setUsername(String username) {
+        this.username = username;
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+}
